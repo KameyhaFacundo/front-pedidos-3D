6893274771ba6f3afdd42f30c3262113c945a4f5
Facundo Kameyha <kameyhafacundo@gmail.com>
2026-08-12 02:14:53 -0300

    Agrego scale

--- src/components/ARViewer.jsx
+++ src/components/ARViewer.jsx
@@ -90,7 +90,7 @@ export default function ARViewer({ plato, onClose, onAddToCart }) {
           ar-scale="fixed"
           camera-controls
           auto-rotate
-          scale="0.007 0.007 0.007"
+          scale="0.009 0.009 0.009"
           style={{
             width: '100%',
             height: '100%',
